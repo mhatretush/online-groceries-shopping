fb088880218c2a57a94f641c048f9bbc355c32b9
Aditi-Ghodke <aditighodke09@gmail.com>
2025-10-28 11:26:12 +0530

    added user, order entities

--- src/main/java/com/ogs/shopping/entity/Order.java
+++ src/main/java/com/ogs/shopping/entity/Order.java
@@ -1,4 +1,28 @@
 package com.ogs.shopping.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private double totalAmount;
+    private double discountAmount;
+    private double payableAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
