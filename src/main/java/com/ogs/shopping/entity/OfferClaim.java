ece76edd12f78377c57f429a93c6243bd997de28
Vaibhav010 <vaibhavchavan034@gmail.com>
2025-10-28 12:43:57 +0530

    offerClaim entity with offer and user relation

--- src/main/java/com/ogs/shopping/entity/OfferClaim.java
+++ src/main/java/com/ogs/shopping/entity/OfferClaim.java
@@ -1,4 +1,39 @@
 package com.ogs.shopping.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="offerClaims", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "offer_id"})
+})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OfferClaim {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long offerClaimId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId",nullable = false)
+    private User user;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="offerId", nullable = false)
+    private Offer offer;
+
+    @Column(nullable = false)
+    private LocalDate claimedAt;
+
+    @Column(nullable = false)
+    private boolean successful;
+
+
+
 }
