4e517c1214b0d225bef3c560f78c0c012a8ed765
Vaibhav010 <vaibhavchavan034@gmail.com>
2025-10-30 16:17:18 +0530

    hello

--- src/main/java/com/ogs/shopping/repository/OfferClaimRepository.java
+++ src/main/java/com/ogs/shopping/repository/OfferClaimRepository.java
@@ -1,10 +1,22 @@
-//package com.ogs.shopping.repository;
-//
-//import com.ogs.shopping.entity.Offer;
-//import com.ogs.shopping.entity.OfferClaim;
-//import com.ogs.shopping.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface OfferClaimRepository extends JpaRepository<OfferClaim,Long> {
-//    boolean existsByUserIdAndOfferId(User userId, Offer offerId);
-//}
+package com.ogs.shopping.repository;
+
+import com.ogs.shopping.entity.Offer;
+import com.ogs.shopping.entity.OfferClaim;
+import com.ogs.shopping.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OfferClaimRepository extends JpaRepository<OfferClaim,Long> {
+    boolean existsByUserAndOffer(User user, Offer offer);
+
+
+    Optional<OfferClaim> findByUserAndOffer(User user, Offer offer);
+
+
+    List<OfferClaim> findByUser_UserId(Long userId);
+
+
+    List<OfferClaim> findByOffer_OfferId(Long offerId);
+}
